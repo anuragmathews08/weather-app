543de6d8c9cf2a4f19b186371a880062aeb886a2
anuragmathews08 <anuragmathews619@gmail.com>
2022-07-05 22:04:38 +0530

    Details component UI developed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { ChakraProvider, theme, Grid } from '@chakra-ui/react';
+import { ChakraProvider, theme, Grid, Box } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import Header from './container/Header';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Grid>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <Header />
-      </Grid>
+      <Box  bg="200" h='100vh'>
+        <Grid>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <Header />
+        </Grid>
+      </Box>
     </ChakraProvider>
   );
 }
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,17 +1,96 @@
-import {Box, Button, Collapse, useDisclosure} from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Collapse,
+  useDisclosure,
+  Heading,
+} from '@chakra-ui/react';
 
 function Details() {
-    const { isOpen, onToggle } = useDisclosure();
-    return (
-        <Box mt={4}>
-            <Button onClick={onToggle}>{!isOpen ? "Show Details" : "Hide Details"}</Button>
-            <Collapse in={isOpen} animateOpacity>
-                <Box>
-                    <h1>This is Collapse test</h1>
-                </Box>
-            </Collapse>
+  const { isOpen, onToggle } = useDisclosure();
+  return (
+    <Box mt={4}>
+      <Button
+        onClick={onToggle}
+        w={['100vw', '70vw', '65vw']}
+        border="1px"
+        borderColor="gray.200"
+      >
+        {!isOpen ? 'Show Details' : 'Hide Details'}
+      </Button>
+      <Collapse in={isOpen} animateOpacity>
+        <Box
+          w={['100vw', '70vw', '65vw']}
+          h={['7rem', '9rem', '11rem']}
+          pt={2}
+          display="flex"
+          justifyContent="center"
+        >
+          <Box
+            w={['100%', '80%']}
+            h="6rem"
+            boxShadow="lg"
+            borderRadius="lg"
+            display="flex"
+            justifyContent="space-evenly"
+            alignItems="center"
+            border="1px"
+            borderColor="gray.100"
+          >
+            <Box
+              w="33%"
+              h="80%"
+              borderRight="1px"
+              borderColor="gray.200"
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent='space-evenly'
+            >
+              <Heading as="h6" size="sm" color="blackAlpha.500">
+                Wind
+              </Heading>
+              <Heading as="h6" size="md">
+                32km/h
+              </Heading>
+            </Box>
+            <Box
+              w="33%"
+              h="80%"
+              borderRight="1px"
+              borderColor="gray.200"
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent='space-evenly'
+            >
+              <Heading as="h6" size="sm" color="blackAlpha.500">
+                Humidity
+              </Heading>
+              <Heading as="h6" size="md">
+                23 km
+              </Heading>
+            </Box>
+            <Box
+              w="33%"
+              h="80%"
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent='space-evenly'
+            >
+              <Heading as="h6" size="sm" color="blackAlpha.500">
+                Visibility
+              </Heading>
+              <Heading as="h6" size="md">
+                64%
+              </Heading>
+            </Box>
+          </Box>
         </Box>
-    );
+      </Collapse>
+    </Box>
+  );
 }
 
-export default  Details;
+export default Details;
